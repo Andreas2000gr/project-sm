3c415ccb82df92aa32643d4f4bd905dd5527aa24
andreas2000gr@gmail.com <andreas2000gr@gmail.com@1c455c11-1ea8-28e4-6a39-140aa87a5f92>
2014-02-06 00:20:16 +0000

    Initialize the new repo

---
+++ trunk/src/External_System/ExternalBank.java
@@ -0,0 +1,112 @@
+
+package External_System;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Euh
+ */
+@Entity
+@Table(name = "EXTERNAL_BANK")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "ExternalBank.findAll", query = "SELECT e FROM ExternalBank e"),
+    @NamedQuery(name = "ExternalBank.findByBankId", query = "SELECT e FROM ExternalBank e WHERE e.bankId = :bankId"),
+    @NamedQuery(name = "ExternalBank.findByName", query = "SELECT e FROM ExternalBank e WHERE e.name = :name")})
+public class ExternalBank implements Serializable {
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "BANK_ID")
+    private Integer bankId;
+    @Column(name = "NAME")
+    private String name;
+    @OneToMany(mappedBy = "bank")
+    private Collection<CreditCardAuthority> creditCardAuthorityCollection;
+
+    public ExternalBank() {
+    }
+
+    public ExternalBank(Integer bankId) {
+        this.bankId = bankId;
+    }
+
+    public Integer getBankId() {
+        return bankId;
+    }
+
+    public void setBankId(Integer bankId) {
+        Integer oldBankId = this.bankId;
+        this.bankId = bankId;
+        changeSupport.firePropertyChange("bankId", oldBankId, bankId);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        String oldName = this.name;
+        this.name = name;
+        changeSupport.firePropertyChange("name", oldName, name);
+    }
+
+    @XmlTransient
+    public Collection<CreditCardAuthority> getCreditCardAuthorityCollection() {
+        return creditCardAuthorityCollection;
+    }
+
+    public void setCreditCardAuthorityCollection(Collection<CreditCardAuthority> creditCardAuthorityCollection) {
+        this.creditCardAuthorityCollection = creditCardAuthorityCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (bankId != null ? bankId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ExternalBank)) {
+            return false;
+        }
+        ExternalBank other = (ExternalBank) object;
+        if ((this.bankId == null && other.bankId != null) || (this.bankId != null && !this.bankId.equals(other.bankId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "External_System.ExternalBank[ bankId=" + bankId + " ]";
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+    
+}
---
+++ trunk/src/supermarket/Pojos/Store.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package supermarket.Pojos;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Euh
+ */
+@Entity
+@Table(name = "STORE")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Store.findAll", query = "SELECT s FROM Store s"),
+    @NamedQuery(name = "Store.findByStoreId", query = "SELECT s FROM Store s WHERE s.storeId = :storeId"),
+    @NamedQuery(name = "Store.findByName", query = "SELECT s FROM Store s WHERE s.name = :name"),
+    @NamedQuery(name = "Store.findByAddress", query = "SELECT s FROM Store s WHERE s.address = :address")})
+public class Store implements Serializable {
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "STORE_ID")
+    private Integer storeId;
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "ADDRESS")
+    private String address;
+    @ManyToMany(mappedBy = "storeCollection")
+    private Collection<Product> productCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "store")
+    private Collection<Purchase> purchaseCollection;
+
+    public Store() {
+    }
+
+    public Store(Integer storeId) {
+        this.storeId = storeId;
+    }
+
+    public Integer getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Integer storeId) {
+        Integer oldStoreId = this.storeId;
+        this.storeId = storeId;
+        changeSupport.firePropertyChange("storeId", oldStoreId, storeId);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        String oldName = this.name;
+        this.name = name;
+        changeSupport.firePropertyChange("name", oldName, name);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        String oldAddress = this.address;
+        this.address = address;
+        changeSupport.firePropertyChange("address", oldAddress, address);
+    }
+
+    @XmlTransient
+    public Collection<Product> getProductCollection() {
+        return productCollection;
+    }
+
+    public void setProductCollection(Collection<Product> productCollection) {
+        this.productCollection = productCollection;
+    }
+
+    @XmlTransient
+    public Collection<Purchase> getPurchaseCollection() {
+        return purchaseCollection;
+    }
+
+    public void setPurchaseCollection(Collection<Purchase> purchaseCollection) {
+        this.purchaseCollection = purchaseCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (storeId != null ? storeId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Store)) {
+            return false;
+        }
+        Store other = (Store) object;
+        if ((this.storeId == null && other.storeId != null) || (this.storeId != null && !this.storeId.equals(other.storeId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "supermarket.Pojos.Store[ storeId=" + storeId + " ]";
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+    
+}
