0873172b89167865b56597a128d697a8a6df5925
panagis.loukatos@gmail.com <panagis.loukatos@gmail.com@1c455c11-1ea8-28e4-6a39-140aa87a5f92>
2014-02-08 22:27:34 +0000

    Added DB manager and marked 1st Alpha stage.

--- src/supermarket/DBmanager.java
+++ src/supermarket/DBmanager.java
@@ -9,13 +9,40 @@ package supermarket;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.FlushModeType;
 import javax.persistence.Persistence;
 
 /**
  *
  * @author Euh
  */
+     
+
 public class DBmanager {
+     
+    private EntityManager loc;
+    private EntityManager ext;
     
+    public DBmanager(){
+        EntityManagerFactory emLoc = Persistence.createEntityManagerFactory("SuperMarket-local-PU");
+        EntityManagerFactory emExt = Persistence.createEntityManagerFactory("SuperMarket-external-PU");
+       
+        loc = emLoc.createEntityManager();  
+        ext = emExt.createEntityManager();
+    }
+
+    public EntityManager getLoc() {
+        return loc;
+    }
+
+    public void setLoc(EntityManager loc) {
+        this.loc = loc;
+    }
+
+    public EntityManager getExt() {
+        return ext;
+    }
+
+    public void setExt(EntityManager ext) {
+        this.ext = ext;
+    }
 }
