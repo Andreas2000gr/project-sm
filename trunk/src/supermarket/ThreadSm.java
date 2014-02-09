ac0e41d3dcbdf3f0bb0db6a8cfbead01bfc4dd84
andreas2000gr@gmail.com <andreas2000gr@gmail.com@1c455c11-1ea8-28e4-6a39-140aa87a5f92>
2014-02-09 01:36:21 +0000

    ver 0.01

---
+++ trunk/src/supermarket/ThreadSm.java
@@ -0,0 +1,23 @@
+package supermarket;
+
+
+import LocalDB.Customer;
+
+
+public class ThreadSm extends Thread {
+    
+     private DBmanager db;
+     private Customer customer;
+    
+}
+
+//Constructor gia ThreadSm
+
+//Μεθοδος για δημιουργια καλαθιού με τυχαίες αγορές <20 προιοντα
+
+//Μεθοδος για καταχωρηση του καλαθιού σαν αγορά στη βάση
+
+//Μεθοδος για σύνδεση με αρχη πιστοποίησης 
+
+//Μεθοδος για έλεγχος των thread συμφωνα με την εκφωνηση
+
