c87b3029a3481165eea52d5ca9a07b75e0923716
epapavasileiou@gmail.com <epapavasileiou@gmail.com@1c455c11-1ea8-28e4-6a39-140aa87a5f92>
2014-02-10 20:04:05 +0000

    CUSTOMER_UPDATE_PASSWORD

--- trunk/src/supermarket/SuperMarket.java
+++ trunk/src/supermarket/SuperMarket.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package supermarket;
+
 /**
  *
  * @author Loukatos
@@ -17,15 +18,15 @@ import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 public class SuperMarket {
-    
+
     // Δημιουργία του ενιαίου DB manager
-    private static DBmanager db = new DBmanager(); 
-    
-     /**
+    private static DBmanager db = new DBmanager();
+
+    /**
      * @param args the command line arguments
      */
     public SuperMarket() {
-        
+
     }
 
     // @EPA:: Καθαρίζει όλους τους πίνακες της ΒΔ     
@@ -52,14 +53,14 @@ public class SuperMarket {
             db.getLoc().getTransaction().rollback();
         }
     }
-    
+
     private static void createAndShowGUI() {
         SuperMarketParentFrame s = new SuperMarketParentFrame();
         s.setVisible(true);
         s.pack();
     }
 
-     /**
+    /**
      * @EPA:: Δημιουργεί προϊόντα και καταστήματα καθορίζει τα προϊόντα που
      * εμπορεύεται κάθε κατάστημα και τα αποθηκεύει στη ΒΔ.
      */
@@ -67,38 +68,37 @@ public class SuperMarket {
         // αρχικοποίηση transaction
         db.getLoc().getTransaction().begin();
         try {
-            /* Δημιουργία Προϊόντων */ 
-            Product odokrema = new Product(null,"Aim Οδοντόκρεμα 75 ml", "1100", 10, 4.35f);
-            Product makaroniano7 = new Product(null,"Μακαρόνια No7", "1110", 5, 2.35f);
-            Product alevri = new Product(null,"Αλεύρι Ολικής", "1120", 4, 8.88f);
-            Product ladi = new Product(null,"Λάδι 5lt", "1130", 3, 22.34f);
-            Product dimitriaka = new Product(null,"Δημητριακά ολικής", "1140", 2, 4.61f);
-            Product xartikouzinas = new Product(null,"Χαρτί κουζίνας", "1150", 50, 4.66f);
-            Product makaroniano3 = new Product(null,"Μακαρόνια No3", "1160", 20, 4.71f);
-            Product badedas = new Product(null,"Badedas Αφρόλουτρο 750 ml", "1170", 15, 4.76f);
-            Product kafesellinikos = new Product(null,"Καφές Ελλ. 250gr", "1180", 12, 4.81f);
-            Product kafesfiltrou = new Product(null,"Καφές Φίλτρου 250gr", "1190", 10, 4.86f);
-            Product kafesespresso = new Product(null,"Καφές espresso 250gr", "1200", 5, 4.91f);
-            Product aporrouxwn = new Product(null,"Απορρυπαντικό Ρούχων", "1210", 16, 24.96f);
-            Product kyboimaggi = new Product(null,"Maggi Κύβοι Ζωμό 12τεμ.", "1220", 16, 5.01f);
-            Product moustarda = new Product(null,"Μουστάρδα 500 gr", "1230", 17, 5.06f);
-            Product ketsap = new Product(null,"Κέτσαπ 500 gr", "1240", 17, 5.11f);
-            Product malaktikoroux = new Product(null,"Μαλακτικό Ρούχων 1lt", "1250", 18, 15.16f);
-            Product ryzibasmati = new Product(null,"Ρύζι Basmati 500gr", "1260", 18, 5.21f);
-            Product tsixles = new Product(null,"Τσίχλες Trdent Μέντα", "1270", 19, 5.26f);
-            Product galafresko = new Product(null,"Γάλα φρέσκο 1Ltr", "1280", 20, 5.31f);
-            Product sokolata = new Product(null,"Σολολάτες Lakta 12τμ.", "1290", 20, 15.21f);
+            /* Δημιουργία Προϊόντων */
+            Product odokrema = new Product(null, "Aim Οδοντόκρεμα 75 ml", "1100", 10, 4.35f);
+            Product makaroniano7 = new Product(null, "Μακαρόνια No7", "1110", 5, 2.35f);
+            Product alevri = new Product(null, "Αλεύρι Ολικής", "1120", 4, 8.88f);
+            Product ladi = new Product(null, "Λάδι 5lt", "1130", 3, 22.34f);
+            Product dimitriaka = new Product(null, "Δημητριακά ολικής", "1140", 2, 4.61f);
+            Product xartikouzinas = new Product(null, "Χαρτί κουζίνας", "1150", 50, 4.66f);
+            Product makaroniano3 = new Product(null, "Μακαρόνια No3", "1160", 20, 4.71f);
+            Product badedas = new Product(null, "Badedas Αφρόλουτρο 750 ml", "1170", 15, 4.76f);
+            Product kafesellinikos = new Product(null, "Καφές Ελλ. 250gr", "1180", 12, 4.81f);
+            Product kafesfiltrou = new Product(null, "Καφές Φίλτρου 250gr", "1190", 10, 4.86f);
+            Product kafesespresso = new Product(null, "Καφές espresso 250gr", "1200", 5, 4.91f);
+            Product aporrouxwn = new Product(null, "Απορρυπαντικό Ρούχων", "1210", 16, 24.96f);
+            Product kyboimaggi = new Product(null, "Maggi Κύβοι Ζωμό 12τεμ.", "1220", 16, 5.01f);
+            Product moustarda = new Product(null, "Μουστάρδα 500 gr", "1230", 17, 5.06f);
+            Product ketsap = new Product(null, "Κέτσαπ 500 gr", "1240", 17, 5.11f);
+            Product malaktikoroux = new Product(null, "Μαλακτικό Ρούχων 1lt", "1250", 18, 15.16f);
+            Product ryzibasmati = new Product(null, "Ρύζι Basmati 500gr", "1260", 18, 5.21f);
+            Product tsixles = new Product(null, "Τσίχλες Trdent Μέντα", "1270", 19, 5.26f);
+            Product galafresko = new Product(null, "Γάλα φρέσκο 1Ltr", "1280", 20, 5.31f);
+            Product sokolata = new Product(null, "Σολολάτες Lakta 12τμ.", "1290", 20, 15.21f);
 
 
             /* Δημιουργία Καταστημάτων */
-            Store abAlimou = new Store(null,"AB Αλίμου", "Καλαμακίου 120, Άλιμος");
-            Store abFalirou = new Store(null,"AB Φαλήρου", "Ποσειδώνος 300, Παλαιό Φάληρο");
-            Store abGeraka = new Store(null,"AB Γέρακα", "Λεωφόρος Σπάτων 81, Γέρακας");
-            
+            Store abAlimou = new Store(null, "AB Αλίμου", "Καλαμακίου 120, Άλιμος");
+            Store abFalirou = new Store(null, "AB Φαλήρου", "Ποσειδώνος 300, Παλαιό Φάληρο");
+            Store abGeraka = new Store(null, "AB Γέρακα", "Λεωφόρος Σπάτων 81, Γέρακας");
+
             //Παρακάτω φαίνεται ο σωστός τρόπος ώστε να ενημερωθεί και ο πίνακας 
             //Store_Product. O λόγος που δεν δουλεύει το ανάποδο είναι διότι η κλάση Product
             // έχει τα στοιχεία για τον join table και μόνο μία εκ των δύο μπορεί να τα έχει.
-
             Collection<Store> sc = new ArrayList<>(); //αρχικοποίηση
             sc.add(abGeraka);
             ketsap.setStoreCollection(sc);
@@ -174,7 +174,6 @@ public class SuperMarket {
 //            abGeraka.setProductCollection(GerakaList); 
 //            db.getLoc().persist(abGeraka);
 //            db.getLoc().merge(abGeraka);
-
             /**
              * Κάνοντας commit το transaction θα δημιουργηθούν οι αντίστοιχες
              * εγγραφές Store και Product στη ΒΔ ενώ τα αντίστοιχα αντικείμενα
@@ -186,13 +185,13 @@ public class SuperMarket {
             db.getLoc().getTransaction().rollback();
         }
     }
-    
+
     public static void main(String[] args) {
         // Δημιουργούμε το SuperMarket
         SuperMarket sm = new SuperMarket();
         sm.CleanDB();
         sm.createStoresAndProducts();
-        
+
         //Schedule a job for the event dispatch thread:
         //creating and showing this application's GUI.
         SwingUtilities.invokeLater(new Runnable() {
@@ -203,5 +202,20 @@ public class SuperMarket {
             }
         });
 
+    }
+
+    // METHOD:: UPDATE CUSTOMER'S PASSWORD
+    public boolean CUSTOMER_UPDATE_PASSWORD(Customer customer) {
+        try {
+            // αρχικοποίηση transaction
+            db.getLoc().getTransaction().begin();
+            db.getLoc().persist(customer);
+            db.getLoc().getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            db.getLoc().getTransaction().rollback();
+            return false;
+        }
     }
 }
