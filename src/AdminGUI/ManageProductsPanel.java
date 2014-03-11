ea6c5ba361fe6dace0a19d4b6ea271659275869d
panagis.loukatos@gmail.com <panagis.loukatos@gmail.com@1c455c11-1ea8-28e4-6a39-140aa87a5f92>
2014-03-11 01:00:28 +0000

    Aesthetic changes

--- src/AdminGUI/ManageProductsPanel.java
+++ src/AdminGUI/ManageProductsPanel.java
@@ -7,11 +7,11 @@
 package AdminGUI;
 
 import LocalDB.Product;
+import LocalDB.Store;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.swing.JOptionPane;
-import javax.swing.table.DefaultTableModel;
 import supermarket.SuperMarketParentFrame;
 
 /**
@@ -30,7 +30,8 @@ public class ManageProductsPanel extends javax.swing.JPanel {
     private String newValue;
     private Integer row;
     private Integer column;
-    private final List<Product> changedProd = new  ArrayList<>();
+    private List<Product> changedProd = new  ArrayList<>();
+    private List<Product> prodList = new ArrayList<>();    
     private Product prod = new Product();
     
     public ManageProductsPanel(SuperMarketParentFrame frame) {
@@ -58,12 +59,13 @@ public class ManageProductsPanel extends javax.swing.JPanel {
         customerList = java.beans.Beans.isDesignTime() ? java.util.Collections.emptyList() : customerQuery.getResultList();
         productQuery = java.beans.Beans.isDesignTime() ? null : entityManager.createQuery("SELECT p FROM Product p");
         productList = java.beans.Beans.isDesignTime() ? java.util.Collections.emptyList() : productQuery.getResultList();
+        storeQuery = java.beans.Beans.isDesignTime() ? null : entityManager.createQuery("SELECT s FROM Store s");
+        storeList = java.beans.Beans.isDesignTime() ? java.util.Collections.emptyList() : storeQuery.getResultList();
         ReturnButton = new javax.swing.JButton();
         SaveButton = new javax.swing.JButton();
         NewButton = new javax.swing.JButton();
         ClearChanges = new javax.swing.JButton();
-        RemoveRow = new javax.swing.JButton();
-        AddRow = new javax.swing.JButton();
+        DeleteButton = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         ProductTable = new javax.swing.JTable();
 
@@ -84,6 +86,11 @@ public class ManageProductsPanel extends javax.swing.JPanel {
         });
 
         NewButton.setText("Δημιουργία");
+        NewButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                NewButtonActionPerformed(evt);
+            }
+        });
 
         ClearChanges.setText("Επαναφορά");
         ClearChanges.addActionListener(new java.awt.event.ActionListener() {
@@ -92,17 +99,10 @@ public class ManageProductsPanel extends javax.swing.JPanel {
             }
         });
 
-        RemoveRow.setText("-");
-        RemoveRow.addActionListener(new java.awt.event.ActionListener() {
+        DeleteButton.setText("Διαγραφή");
+        DeleteButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                RemoveRowActionPerformed(evt);
-            }
-        });
-
-        AddRow.setText("+");
-        AddRow.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                AddRowActionPerformed(evt);
+                DeleteButtonActionPerformed(evt);
             }
         });
 
@@ -117,11 +117,12 @@ public class ManageProductsPanel extends javax.swing.JPanel {
         columnBinding.setColumnName("Price");
         columnBinding.setColumnClass(Float.class);
         bindingGroup.addBinding(jTableBinding);
-        jTableBinding.bind();org.jdesktop.beansbinding.Binding binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, ProductTable, org.jdesktop.beansbinding.ELProperty.create("${selectedElement}"), ProductTable, org.jdesktop.beansbinding.BeanProperty.create("selectedElement"));
-        bindingGroup.addBinding(binding);
-        binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, ProductTable, org.jdesktop.beansbinding.ELProperty.create("${selectedElements}"), ProductTable, org.jdesktop.beansbinding.BeanProperty.create("selectedElements"));
-        bindingGroup.addBinding(binding);
-
+        jTableBinding.bind();
+        ProductTable.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                ProductTableFocusGained(evt);
+            }
+        });
         ProductTable.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
             public void propertyChange(java.beans.PropertyChangeEvent evt) {
                 ProductTablePropertyChange(evt);
@@ -144,25 +145,20 @@ public class ManageProductsPanel extends javax.swing.JPanel {
                         .addComponent(NewButton, javax.swing.GroupLayout.DEFAULT_SIZE, 107, Short.MAX_VALUE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(SaveButton, javax.swing.GroupLayout.DEFAULT_SIZE, 109, Short.MAX_VALUE))
-                    .addComponent(jScrollPane1))
+                    .addComponent(jScrollPane1)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(DeleteButton, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(AddRow, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(RemoveRow, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 276, Short.MAX_VALUE)
-                .addGap(18, 18, 18)
-                .addComponent(AddRow, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(RemoveRow, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(21, 21, 21)
+                .addComponent(DeleteButton)
+                .addGap(62, 62, 62)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -243,15 +239,31 @@ public class ManageProductsPanel extends javax.swing.JPanel {
                 }
     }//GEN-LAST:event_ClearChangesActionPerformed
 
-    private void RemoveRowActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RemoveRowActionPerformed
-
-    }//GEN-LAST:event_RemoveRowActionPerformed
-
-    private void AddRowActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AddRowActionPerformed
-        prod = new Product();
-        productList.add(prod);
-        ProductTable.updateUI();
-    }//GEN-LAST:event_AddRowActionPerformed
+    private void DeleteButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DeleteButtonActionPerformed
+        if (ProductTable.getSelectedRow()!= -1) {
+            for (Integer i : ProductTable.getSelectedRows()) {
+                prodList.add(productList.get(ProductTable.convertRowIndexToModel(i)));
+                tableChanged = true;
+                DeleteButton.setEnabled(false);
+            }
+            productList.removeAll(prodList);
+            ProductTable.updateUI();
+            for (Product c : prodList) {
+                try {
+                    prod = loc.find(Product.class, c.getProductId());
+                    loc.remove(prod);
+                    for (Store store : storeList) {
+                        store.getProductCollection().remove(prod);
+                        loc.merge(store);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    JOptionPane.showMessageDialog(null, "Σφάλμα βάσης...");
+                }
+            }
+            prodList.clear();
+        }  
+    }//GEN-LAST:event_DeleteButtonActionPerformed
 
     private void ProductTablePropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_ProductTablePropertyChange
         if ("tableCellEditor".equals(evt.getPropertyName())) {
@@ -270,13 +282,46 @@ public class ManageProductsPanel extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_ProductTablePropertyChange
 
+    private void ProductTableFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_ProductTableFocusGained
+        DeleteButton.setEnabled(true); //Η διαγραφή ενεργοποιείται μονο εάν έχουμε επιλέξει σειρά.
+    }//GEN-LAST:event_ProductTableFocusGained
+
+    private void NewButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NewButtonActionPerformed
+        if (tableChanged) {
+            Object[] options = {"Ναι","Οχι"};
+            Integer choice = JOptionPane.showOptionDialog(null,
+            "Επιβεβαίωση αλλαγών;",
+            null,
+            JOptionPane.YES_NO_OPTION,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            options,
+            options[0]);
+
+            if (choice == JOptionPane.YES_OPTION){
+                if (loc.getTransaction().isActive()) {
+                    try {
+                        loc.getTransaction().commit();
+                    } catch (Exception e) {
+                            e.printStackTrace();
+                        loc.getTransaction().rollback(); 
+                    }
+                }
+            }
+        }
+        
+        CreateProductDialog dialog = new CreateProductDialog(frame,true);
+        dialog.setVisible(true);
+        frame.pnl = new ManageProductsPanel(frame);
+        frame.addPanelInMain();
+    }//GEN-LAST:event_NewButtonActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton AddRow;
     private javax.swing.JButton ClearChanges;
+    private javax.swing.JButton DeleteButton;
     private javax.swing.JButton NewButton;
     private javax.swing.JTable ProductTable;
-    private javax.swing.JButton RemoveRow;
     private javax.swing.JButton ReturnButton;
     private javax.swing.JButton SaveButton;
     private java.util.List<LocalDB.Customer> customerList;
@@ -285,6 +330,8 @@ public class ManageProductsPanel extends javax.swing.JPanel {
     private javax.swing.JScrollPane jScrollPane1;
     private java.util.List<LocalDB.Product> productList;
     private javax.persistence.Query productQuery;
+    private java.util.List<LocalDB.Store> storeList;
+    private javax.persistence.Query storeQuery;
     private org.jdesktop.beansbinding.BindingGroup bindingGroup;
     // End of variables declaration//GEN-END:variables
 }
